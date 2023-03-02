6d7a2372dd078cbca41c0ba8a0ec15f8244d63bd
Jevria <jevriya.guliyeva@gmail.com>
2023-03-02 14:33:41 +0000

    Add pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,40 +4,50 @@ import Categories from "../components/Categories/Categories";
 import Sort from "../components/Sort";
 import Skeleton from "../components/Skeleton";
 import PizzaBlock from "../components/PizzaBlock/PizzaBlock";
-import { SearchContext } from "../search-context";
+import Pagination from "../components/Pagination/Pagination";
 
+
+import { SearchContext } from "../search-context";
 function Home() {
     const {searchValue, setSearchValue} = useContext(SearchContext)
 
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    const [errorMessage, setErrorMessage] = useState(true);
+    const [currentPage, setCurrentPage] = useState(1);
     const [categoryId, setCategoryId] = useState(0);
     const [sortId, setSortId] = useState({name: 'популярности', sortProperty:"rating" });
 
     const skeleton = [...new Array(6)].map((_, index) => (
         <Skeleton key={index} />
     ));
-    // const pizzas = data.filter(pizzaName =>{
-    //     pizzaName.toLowerCase().includes(data.toLowerCase())
+    const pizzas = data
+    //     .filter( pizzaName => {
+    //     if(pizzaName.title.toLowerCase().includes(searchValue.toLowerCase())){
+    //         return true
+    //     }
+    //         return false
     // })
-    const categoryQuery = categoryId > 0 ? `category = ${categoryId}` : '';
+        .map((pizzaItem) => (
+            <PizzaBlock key={pizzaItem.id} {...pizzaItem} />
+        ))
+    const categoryQuery = categoryId > 0 ? `&category = ${categoryId}` : '';
 // &sortBy=${sortId.sortProperty}&order=desc
+
+    const search = searchValue ? `&search = ${searchValue}` : '';
     useEffect(() => {
         setIsLoading(true);
         console.log(categoryId, 'categoryId')
-        fetch(`https://632a346c713d41bc8e6c260d.mockapi.io/items?${categoryQuery}`)
+        fetch(`https://632a346c713d41bc8e6c260d.mockapi.io/items?page=${currentPage}&limit=4${categoryQuery}${search}`)
             .then((response) => response.json())
             .then((response) => {
                 setData(response);
                 setIsLoading(false);
             })
             .catch(() => {
-                setErrorMessage("Unable to fetch item list");
                 setIsLoading(true);
             });
         window.scrollTo(0, 0);
-    }, [categoryId, sortId,categoryQuery]);
+    }, [categoryId, sortId, searchValue, currentPage]);
 
 
     return (
@@ -49,15 +59,10 @@ function Home() {
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
                 {
-                    isLoading
-                    ? [...new Array(6)].map((_, index) => (
-                            <Skeleton key={index} />
-                        ))
-                    : data.map((pizzaItem) => (
-                            <PizzaBlock key={pizzaItem.id} {...pizzaItem} />
-                        ))
+                    isLoading ? skeleton : pizzas
                 }
             </div>
+            <Pagination onChangePage={(num) => setCurrentPage(num)} />
         </div>
     );
 }
